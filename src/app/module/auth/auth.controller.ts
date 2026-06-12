e5abe145223673431d5367cdecf40bcfba891676
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-06-12 21:56:48 +0600

    feat(auth): add login route and service
    - implement loginUser controller handling tokens and response
    - expose login endpoint and service method for authentication

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -39,8 +39,32 @@ const registerPatient = catchAsync(
         })
     }
 )
+const loginUser = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+        const result = await AuthService.loginUser(payload);
+        const { accessToken, refreshToken, token, ...rest } = result
 
+        tokenUtils.setAccessTokenCookie(res, accessToken);
+        tokenUtils.setRefreshTokenCookie(res, refreshToken);
+        tokenUtils.setBetterAuthSessionCookie(res, token);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "User logged in successfully",
+            data: {
+                token,
+                accessToken,
+                refreshToken,
+                ...rest,
+
+            },
+        })
+    }
+)
 
 export const AuthController = {
-    registerPatient
+    registerPatient,
+    loginUser
 };
--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -85,9 +85,54 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
 
 }
 
+const loginUser = async (payload: ILoginUserPayload) => {
+    const { email, password } = payload;
 
+    const data = await auth.api.signInEmail({
+        body: {
+            email,
+            password,
+        }
+    })
+
+    if (data.user.status === UserStatus.BLOCKED) {
+        throw new AppError(status.FORBIDDEN, "User is blocked");
+    }
+
+    if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+        throw new AppError(status.NOT_FOUND, "User is deleted");
+    }
+
+    const accessToken = tokenUtils.getAccessToken({
+        userId: data.user.id,
+        role: data.user.role,
+        name: data.user.name,
+        email: data.user.email,
+        status: data.user.status,
+        isDeleted: data.user.isDeleted,
+        emailVerified: data.user.emailVerified,
+    });
+
+    const refreshToken = tokenUtils.getRefreshToken({
+        userId: data.user.id,
+        role: data.user.role,
+        name: data.user.name,
+        email: data.user.email,
+        status: data.user.status,
+        isDeleted: data.user.isDeleted,
+        emailVerified: data.user.emailVerified,
+    });
+
+    return {
+        ...data,
+        accessToken,
+        refreshToken,
+    };
+
+}
 
 
 export const AuthService = {
-    registerPatient
+    registerPatient,
+    loginUser
 };
