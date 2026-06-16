cf93de487634973a7bae9803eea5edfc31ec860b
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-06-16 22:38:46 +0600

    feat(auth): add forgetPassword method
    - validates user existence and email verification
    - triggers password reset OTP request
    - integrates method into AuthService exports

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -322,6 +322,32 @@ const verifyEmail = async (email : string, otp : string) => {
     }
 }
 
+const forgetPassword = async (email : string) => {
+    const isUserExist = await prisma.user.findUnique({
+        where : {
+            email,
+        }
+    })
+
+    if(!isUserExist){
+        throw new AppError(status.NOT_FOUND, "User not found");
+    }
+
+    if(!isUserExist.emailVerified){
+        throw new AppError(status.BAD_REQUEST, "Email not verified");
+    }
+
+    if(isUserExist.isDeleted || isUserExist.status === UserStatus.DELETED){
+        throw new AppError(status.NOT_FOUND, "User not found"); 
+    }
+
+    await auth.api.requestPasswordResetEmailOTP({
+        body:{
+            email,
+        }
+    })
+}
+
 export const AuthService = {
     registerPatient,
     loginUser,
@@ -329,5 +355,6 @@ export const AuthService = {
     getNewToken,
     changePassword,
     logoutUser,
-    verifyEmail
+    verifyEmail,
+    forgetPassword
 };
